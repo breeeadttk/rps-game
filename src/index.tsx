511e5fb5393fa9b8c30caef4f7cdce1877fd2c9b
oleh.trunov <oleh.trunov@gmail.com>
2023-05-26 13:24:52 +0200

    feature: initialize app

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+// Context
+import { GameProvider } from 'store/GameContext';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <GameProvider>
+      <App />
+    </GameProvider>
+  </React.StrictMode>
+);
